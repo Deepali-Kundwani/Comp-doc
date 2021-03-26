a9a9dc3a586921253b65e96dcb45c85def286a98
Deepali-Kundwani <74810444+Deepali-Kundwani@users.noreply.github.com>
2021-03-26 20:44:04 +0400

    Update Avatar.js

--- src/Components/avatar/Avatar.js
+++ src/Components/avatar/Avatar.js
@@ -24,7 +24,7 @@ export var Avatar = () => {
         />
       </div>
       <Iframe
-        url="https://carbon.now.sh/embed?bg=rgba%28191%2C219%2C254%2C1%29&t=material&wt=none&l=auto&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Fira+Code&fs=14px&lh=152%25&si=false&es=2x&wm=false&code=%253Cimg%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520className%253D%2522avatar-container-item%2522%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520alt%253D%2522img%2522%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520src%253D%2522.%252Fimages%252Fimage-victor.jpg%2522%250A%2520%2520%2520%2520%2520%2520%2520%2520%252F%253E"
+        url="https://carbon.now.sh/embed?bg=rgba%28191%2C219%2C254%2C1%29&t=material&wt=none&l=auto&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Fira+Code&fs=14px&lh=152%25&si=false&es=2x&wm=false&code=%2520%253Cimg%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520class%253D%2522avatar-container-item%2522%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520alt%253D%2522img%2522%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520src%253D%2522.%252Fimages%252Fimage-victor.jpg%2522%250A%2520%2520%2520%2520%2520%2520%2520%2520%252F%253E"
         width="100%"
         height="520px"
         border="0"
