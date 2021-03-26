31d552216f697190d133151d223034f07b0a7457
Deepali-Kundwani <74810444+Deepali-Kundwani@users.noreply.github.com>
2021-03-26 22:00:04 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React, { useState } from "react";
 import { Header } from "./Components/header/Header";
 import { Navigation } from "./Components/navigation/Navigation";
 import { Avatar } from "./Components/avatar/Avatar";
-
+import { Home } from "./Components/home/Home";
 import { Button } from "./Components/button/Button";
 
 export default function App() {
-  const [a, setA] = useState(<Button />);
+  const [a, setA] = useState(<Home />);
 
   return (
     <div className="App-grid">
